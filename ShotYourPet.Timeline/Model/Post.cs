229dd1562feb01c84a3dc7d0e8417b2e128727cf
Aymeric SERRA <oupson1er@gmail.com>
2025-03-13 11:14:57 +0100

    feat: Add fields to Post DTO.

--- ShotYourPet.Timeline/Model/Post.cs
+++ ShotYourPet.Timeline/Model/Post.cs
@@ -2,6 +2,15 @@ namespace ShotYourPet.Timeline.Model;
 
 public class Post
 {
-    public long Id { get; set; }
-    public Guid AuthorId { get; set; }
+    public required long Id { get; init; }
+
+    public required Guid AuthorId { get; init; }
+
+    public required long ChallengeId { get; init; }
+
+    public required DateTimeOffset PublishedAt { get; init; }
+
+    public required string? Content { get; init; }
+
+    public required long ImageId { get; init; }
 }
