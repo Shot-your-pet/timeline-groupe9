b977b3c5612b70000719aa40c3456eac99d9780a
Aymeric SERRA <oupson1er@gmail.com>
2025-04-04 20:11:02 +0200

    feat: add event queue to fix threading issues.

--- ShotYourPet.Messaging/Interfaces.fs
+++ ShotYourPet.Messaging/Interfaces.fs
@@ -2,6 +2,7 @@ namespace ShotYourPet.Messaging
 
 open System
 open System.Threading
+open System.Threading.Channels
 open System.Threading.Tasks
 open ShotYourPet.Messaging.Model
 
@@ -9,3 +10,13 @@ module Interfaces =
     type IUserRpcClient =
         abstract member QueryInformationAsync:
             userId: Guid -> cancellationToken: CancellationToken -> Task<UserInformation>
+
+
+    type EventQueue =
+        { NewPublicationChannel: Channel<NewPublication> }
+
+    let newEventQueue () =
+        { NewPublicationChannel = Channel.CreateUnbounded() }
+
+    let postPublicationEvent eventQueue publication =
+        task { do! eventQueue.NewPublicationChannel.Writer.WriteAsync(publication) }
